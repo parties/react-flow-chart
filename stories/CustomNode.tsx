ee0b753d4d9baca6d26de956c49c1d9d1fef347a
Eric Ogden <eogden@maana.io>
2020-03-15 14:29:24 -0700

    editable node labels!

--- stories/CustomNode.tsx
+++ stories/CustomNode.tsx
@@ -1,10 +1,10 @@
-import { cloneDeep, mapValues, get, throttle } from 'lodash'
-import * as React from 'react'
-import styled from 'styled-components'
-import { FlowChart, ILinkDefaultProps, INodeDefaultProps, INodeInnerDefaultProps, LinkDefault, IChart, IOnCanvasClick } from '../src'
-import * as actions from '../src/container/actions'
-import { Page } from './components'
-import { chartSimple } from './misc/exampleChartState'
+import { cloneDeep, mapValues, get, throttle, mergeWith } from 'lodash';
+import * as React from 'react';
+import styled from 'styled-components';
+import { FlowChart, ILinkDefaultProps, INodeDefaultProps, INodeInnerDefaultProps, LinkDefault, IChart, IOnCanvasClick } from '../src';
+import * as actions from '../src/container/actions';
+import { Page } from './components';
+import { chartSimple } from './misc/exampleChartState';
 
 
 // const DarkBox = styled.div`
@@ -20,128 +20,23 @@ const LightBox = styled.div`
   color: #000;
   border-radius: 10px;
   border: 2px solid rgba(100, 100, 100, 0.5);
-`
+`;
 
 const Outer = styled.div`
   padding: 30px;
-`
+`;
 
 const Input = styled.input`
   padding: 10px;
   border: 1px solid cornflowerblue;
   width: 100%;
-`
+`;
 
-const useInput = (initialValue: string) => {
-  const [value, setValue] = React.useState(initialValue)
 
-  return {
-    value,
-    setValue,
-    reset: () => setValue(''),
-    bind: {
-      value,
-      onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(event.target.value)
-      }
-    }
-  }
-}
-
-// https://dev.to/stanleyjovel/simplify-controlled-components-with-react-hooks-23nn
-const useInputChange = () => {
-  const [input, setInput] = React.useState({})
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setInput({
-    ...input,
-    [e.currentTarget.name]: e.currentTarget.value,
-  })
-
-  return [input, handleInputChange]
-}
-
-const EditableLabel = ({ defaultLabel }: { defaultLabel?: string }) => {
-  const [isEditing, setIsEditing] = React.useState(false);
-  const [label, setLabel] = React.useState(defaultLabel);
-
-  return (
-    <>
-      <p>label: {label}</p>
-      {
-        isEditing ? (
-          <input
-            type="text"
-            value={label}
-            onChange={(e) => {
-              e.persist()
-              setLabel(e.target.value)
-            }}
-          />
-        ) : <p onDoubleClick={() => setIsEditing(true)}>dbl click me: {label}</p>
-      }
-    </>
-  );
-};
-
-/**
- * Create the custom component,
- * Make sure it has the same prop signature
- */
-const NodeInnerCustom = ({ node, config }: INodeInnerDefaultProps) => {
-  const [label, setLabel] = React.useState(get(node, 'properties.label'))
-  const [isEditing, setIsEditing] = React.useState(false)
-
-  if (node.type === 'output-only') {
-    return (
-      <Outer>
-        {
-          isEditing ? (
-            <input
-              type="text"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                // console.log(e.nativeEvent);
-                // e.stopPropagation();
-              }}
-              onKeyDown={(e: React.KeyboardEvent) => {
-                console.log('keydown', e.nativeEvent);
-                if (e.key === '8') {
-                  e.stopPropagation();
-                }
-
-                if (e.which === 13) {
-                  alert('submitted');
-                }
-              }}
-              onClick={(e) => e.stopPropagation()}
-              onMouseUp={(e) => e.stopPropagation()}
-              onMouseDown={(e) => e.stopPropagation()}
-            />
-          ) : <p onDoubleClick={() => setIsEditing(true)}>dbl click me: {label}</p>
-        }
-      </Outer>
-    )
-  } else {
-    return (
-      <Outer>
-        <p>Add custom displays for each node.type</p>
-        <p>You may need to stop event propagation so your forms work.</p>
-        <br />
-        <Input
-          type="text"
-          placeholder="Some Input"
-          onChange={(e) => console.log(e)}
-          onClick={(e) => e.stopPropagation()}
-          onMouseUp={(e) => e.stopPropagation()}
-          onMouseDown={(e) => e.stopPropagation()}
-        />
-      </Outer>
-    )
-  }
-}
 
 const Label = styled.div`
   position: absolute;
-`
+`;
 
 const Button = styled.div`
   position: absolute;
@@ -163,7 +58,7 @@ const Button = styled.div`
   &:hover {
     box-shadow: 0 10px 20px rgba(0,0,0,.1);
   }
-`
+`;
 
 const LabelContent = styled.div`
   padding: 5px 10px;
@@ -175,7 +70,142 @@ const LabelContent = styled.div`
   justify-content: center;
   font-size: 10px;
   cursor: pointer;
-`
+`;
+
+
+const useInput = (initialValue: string) => {
+  const [value, setValue] = React.useState(initialValue);
+
+  return {
+    value,
+    setValue,
+    reset: () => setValue(''),
+    bind: {
+      value,
+      onChange: (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(event.target.value);
+      },
+    },
+  };
+};
+
+// https://dev.to/stanleyjovel/simplify-controlled-components-with-react-hooks-23nn
+const useInputChange = () => {
+  const [input, setInput] = React.useState({});
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setInput({
+    ...input,
+    [e.currentTarget.name]: e.currentTarget.value,
+  });
+
+  return [input, handleInputChange];
+};
+
+const EditableLabel = ({ defaultLabel }: { defaultLabel?: string }) => {
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [label, setLabel] = React.useState(defaultLabel);
+
+  return (
+    <>
+      <p>label: {label}</p>
+      {
+        isEditing ? (
+          <input
+            type="text"
+            value={label}
+            onChange={(e) => {
+              e.persist();
+              setLabel(e.target.value);
+            }}
+          />
+        ) : <p onDoubleClick={() => setIsEditing(true)}>dbl click me: {label}</p>
+      }
+    </>
+  );
+};
+
+/**
+ * Create the custom component,
+ * Make sure it has the same prop signature
+ */
+const NodeInnerCustom = ({ node, config }: INodeInnerDefaultProps) => {
+  const [label, setLabel] = React.useState(node.properties.label);
+  const [isEditing, setIsEditing] = React.useState(false);
+
+  const chartState = useChartState();
+  const chartDispatch = useChartDispatch();
+
+  const inputRef = React.createRef<HTMLInputElement>();
+
+  React.useEffect(() => {
+    if (inputRef.current && isEditing) {
+      inputRef.current.focus();
+    }
+  }, [inputRef.current, isEditing])
+
+  if (node.type === 'output-only') {
+    return (
+      <Outer>
+        {
+          isEditing ? (
+            <input
+              type="text"
+              ref={inputRef}
+              defaultValue={node.properties.label}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                // console.log(e.nativeEvent);
+                // e.stopPropagation();
+              }}
+              onKeyDown={(e: React.KeyboardEvent) => {
+                console.log('keydown', e.nativeEvent);
+                if (e.key === '8') {
+                  e.stopPropagation();
+                }
+
+                if (e.which === 13) {
+                  chartDispatch(
+                    mergeWith(chartState, {
+                      nodes: {
+                        [node.id]: {
+                          ...node,
+                          properties: {
+                            ...node.properties,
+                            label: inputRef.current && inputRef.current.value,
+                          },
+                        },
+                      },
+                    }),
+                  );
+
+                  setIsEditing(false);
+                }
+              }}
+              onClick={(e) => e.stopPropagation()}
+              onMouseUp={(e) => e.stopPropagation()}
+              onMouseDown={(e) => e.stopPropagation()}
+            />
+          ) : <p onDoubleClick={() => setIsEditing(true)}>{node.properties.label}</p>
+        }
+      </Outer>
+    );
+  } else {
+    return (
+      <Outer>
+        <p>Add custom displays for each node.type</p>
+        <p>You may need to stop event propagation so your forms work.</p>
+        <br />
+        <Input
+          type="text"
+          placeholder="Some Input"
+          onChange={(e) => console.log(e)}
+          onClick={(e) => e.stopPropagation()}
+          onMouseUp={(e) => e.stopPropagation()}
+          onMouseDown={(e) => e.stopPropagation()}
+        />
+      </Outer>
+    );
+  }
+};
 
 /**
  * Create the custom component,
@@ -190,17 +220,18 @@ const NodeCustom = React.forwardRef(({ node, children, ...otherProps }: INodeDef
     <LightBox ref={ref} {...otherProps}>
       {children}
     </LightBox>
-  )
-})
+  );
+});
 
 const LinkCustom = (stateActions: typeof actions) => (props: ILinkDefaultProps) => {
-  const { startPos, endPos, onLinkClick, link } = props
-  const centerX = startPos.x + (endPos.x - startPos.x) / 2
-  const centerY = startPos.y + (endPos.y - startPos.y) / 2
+  const { startPos, endPos, onLinkClick, link } = props;
+  const centerX = startPos.x + (endPos.x - startPos.x) / 2;
+  const centerY = startPos.y + (endPos.y - startPos.y) / 2;
 
-  const [isEditing, setIsEditing] = React.useState(false)
-  const [label, setLabel] = React.useState(get(props, 'link.properties.label') || '')
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [label, setLabel] = React.useState(get(props, 'link.properties.label') || '');
 
+  const chartState = useChartState();
   const dispatch = useChartDispatch();
 
   return (
@@ -213,25 +244,25 @@ const LinkCustom = (stateActions: typeof actions) => (props: ILinkDefaultProps) 
         {isEditing && <input type="text" value={label} onChange={(e) => setLabel(e.target.value)} />}
         <Button
           onClick={(e) => {
-            console.log('clicked button')
-            onLinkClick({ linkId: link.id })
-            stateActions.onDeleteKey({})
-            e.stopPropagation()
+            console.log('clicked button');
+            onLinkClick({ linkId: link.id });
+            stateActions.onDeleteKey({});
+            e.stopPropagation();
           }}
         >
           x
         </Button>
       </Label>
     </>
-  )
-}
+  );
+};
 
 export class CustomNodeDemo extends React.Component {
   public state = cloneDeep(chartSimple);
 
   public render() {
-    const chart = this.state
-    const stateActions = mapValues(actions, (func: any) => (...args: any) => this.setState(func(...args))) as typeof actions
+    const chart = this.state;
+    const stateActions = mapValues(actions, (func: any) => (...args: any) => this.setState(func(...args))) as typeof actions;
     return (
       <Page>
         <FlowChart
@@ -244,7 +275,7 @@ export class CustomNodeDemo extends React.Component {
           }}
         />
       </Page>
-    )
+    );
   }
 }
 
@@ -305,7 +336,7 @@ function useChartDispatch () {
   return context;
 }
 
-function useForceUpdate(){
+function useForceUpdate () {
   const [value, setValue] = React.useState(0); // integer state
   return () => setValue(value => ++value); // update the state to force render
 }
@@ -334,13 +365,13 @@ export const MyNodeDemo = () => {
         }}
       />
     </Page>
-  )
-}
+  );
+};
 
 export const Demo = () => {
   return (
     <ChartProvider>
       <MyNodeDemo />
     </ChartProvider>
-  )
-}
+  );
+};
